f6d756182d47ddef36f03658bf2453e88a89b502
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-08 22:19:20 +0300

    whoops! forgot I have to commit these kinds of exercises! gotta remember this in the future. anyway 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deleteThese) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let x = 0; x <= deleteThese.length; x++) {
+            if (arr[i] === deleteThese[x]) {
+                arr.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
