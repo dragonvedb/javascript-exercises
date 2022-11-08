785e6659aa5e69725863f2edfa8c88a2d5a5c74d
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-08 23:06:03 +0300

    commit
    Implement a condition that finds the higher number between the arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,18 @@
 const sumAll = function(num1, num2) {
     let factorial = 0;
+
+    let minor;
+    let major;
+   
+    if (num1 < num2) {
+        minor = num1;
+        major = num2;
+    } else {
+        minor = num2;
+        major = num1;
+    };
     
-    for (let i = num1; i <= num2; i++) {
+    for (let i = minor; i <= major; i++) {
         factorial += i;
     };
 
