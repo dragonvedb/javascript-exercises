99ad5dbd10f0431ef6be1fbed890f2ebdb6a3fe2
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-08 23:13:21 +0300

    iImplement a condition that throws error if an argument is not an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,8 @@ const sumAll = function(num1, num2) {
     let minor;
     let major;
    
+    if (num1 < 0 || !(Number.isInteger(num1)) || num2 < 0 || !(Number.isInteger(num2))) return 'ERROR';
+    
     if (num1 < num2) {
         minor = num1;
         major = num2;
