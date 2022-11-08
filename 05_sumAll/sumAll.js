0aa0e223db5e8cbb81469a6a886572848635275d
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-08 22:48:27 +0300

    implement a for loop that adds i's until it's as big as the second number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let factorial = 0;
+    
+    for (let i = num1; i <= num2; i++) {
+        factorial += i;
+    };
 
+    return factorial;
 };
 
 // Do not edit below this line
