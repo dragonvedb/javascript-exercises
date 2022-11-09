846d0b7d7f9036836f87903b682384577827dac4
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-09 22:53:00 +0300

    Add an anlogous function for Celsius to Fahrenheit conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -10,7 +10,13 @@ const ftoc = function(f) {
 };
 
 const ctof = function(c) {
-
+  let f = c * (9 / 5) + 32;
+  
+  if (!(Number.isInteger(f))) {
+    f = Number(f.toFixed(1));
+  }
+  
+  return f;
 };
 
 // Do not edit below this line
