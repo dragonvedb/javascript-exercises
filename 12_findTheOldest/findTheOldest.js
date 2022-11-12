59adcdf97d53f0f1db876370c66f35f72190494a
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-12 20:06:07 +0300

    Add a function that finds the oldest person in an array by iterating through objects with reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let highestAge = 0;
+    let oldestPerson = people.reduce(function(oldest, person) {
 
+        let startAge = person.yearOfBirth;
+        let stopAge;
+
+        if (person.yearOfDeath === undefined) {
+            stopAge = new Date().getFullYear();
+        } else {
+            stopAge = person.yearOfDeath;
+        }
+
+        if (stopAge - startAge > highestAge) {
+            highestAge = stopAge - startAge;
+            oldest = person;
+        }
+
+        return oldest;
+
+    })
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
