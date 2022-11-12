41444f4ef78395ebd049cf900045103d35f8accc
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-12 18:38:54 +0300

    Add a function that returns titles of book objects in an array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titlesArray = [];
 
+    for (let i = 0; i < 2; i++) {
+        titlesArray.push(books[i].title);
+    }
+
+    return titlesArray;
 };
 
 // Do not edit below this line
