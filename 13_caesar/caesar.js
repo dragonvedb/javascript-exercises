d0d3b41b1492ef407e3bc3e8f08a70f608f2e392
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-12 23:04:01 +0300

    Preliminary caesar cypher function that doesn't account for overshift

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,30 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+
+   const utcString = [];
+
+    for (let i = 0; i < string.length; i++) {
+        let utcCode = string.charCodeAt(i);
+        utcString.push(utcCode);
+    }
+
+    const newString = [];
+
+    for (const utcCode of utcString) {
+
+        let newChar;
+
+        if (65 <= utcCode <= 90) {
+            newChar = String.fromCharCode(utcCode + shift);
+        } else if (97 <= utcCode <= 122) {
+            newCHar = String.fromCharCode(utcCode + shift);
+        } else {
+            newChar = String.fromCharCode(utcCode);
+        }
+
+        newString.push(newChar);
+    }
+
+    return newString.join();
 
 };
 
