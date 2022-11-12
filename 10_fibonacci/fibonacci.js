ebe5f6e1b12f8423927bb4c9747fcca258727294
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-12 14:48:10 +0300

    Return an error message if input number is negative

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,6 +2,10 @@ const fibonacci = function(num) {
     let lastNum = 0;
     let currentNum = 1;
 
+    if (num <= 0) {
+        return "OOPS"
+    }
+    
     for (let i = 1; i < num; i++) {
         currentNum += lastNum;
         lastNum = currentNum - lastNum;
