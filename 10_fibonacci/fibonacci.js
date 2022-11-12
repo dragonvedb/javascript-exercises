3786fa899071c4b54ce29d5a964c0e1e665eedb5
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-12 14:44:35 +0300

    Add a fucntion that calculates a Fibonacci sequence number of a given position

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let lastNum = 0;
+    let currentNum = 1;
+
+    for (let i = 1; i < num; i++) {
+        currentNum += lastNum;
+        lastNum = currentNum - lastNum;
+    }
+
+    return currentNum
 
 };
 
