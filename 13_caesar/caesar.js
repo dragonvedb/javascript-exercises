a3ab67ea59246b20d105547c2c906999967f4f6c
Daniel Bronsky <dragonvedb@gmail.com>
2022-11-13 20:28:42 +0300

    Add functions that shift utc codes and handle overflows

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -9,18 +9,37 @@ const caesar = function(string, shift) {
 
     const newString = [];
 
-// FINISH SHIFT ANONYMOUS FUNCTIONS THAT WOULD
-// ACCOUNT FOR OVERSHIFT!!!
+    function capitalLet(utcCode, shift) {
+        if ((utcCode + shift) > 90) {
+            return capitalLet(64, (utcCode + shift) - 90);
+        } else if ((utcCode + shift) < 65) {
+            return capitalLet(91, 65 - (utcCode + shift));
+        } else {
+            return utcCode + shift;
+        }
+    }
+
+    function smallLet(utcCode, shift) {
+        if ((utcCode + shift) > 122) {
+            return capitalLet(96, (utcCode + shift) - 122);
+        } else if ((utcCode + shift) < 97) {
+            return capitalLet(123, 97 - (utcCode + shift));
+        } else {
+            return utcCode + shift;
+        }
+    }
 
 
     for (const utcCode of utcString) {
 
         let newChar;
 
-        if (65 <= utcCode <= 90) {
-            newChar = String.fromCharCode(utcCode + shift);
-        } else if (97 <= utcCode <= 122) {
-            newCHar = String.fromCharCode(utcCode + shift);
+        if (65 <= utcCode && utcCode <= 90) {
+            newChar = capitalLet(utcCode, shift); 
+            newChar = String.fromCharCode(newChar);
+        } else if (97 <= utcCode && utcCode <= 122) {
+            newChar = smallLet(utcCode, shift); 
+            newChar = String.fromCharCode(newChar);
         } else {
             newChar = String.fromCharCode(utcCode);
         }
