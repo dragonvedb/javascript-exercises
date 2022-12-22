d6181eed2192ec8d27ead8e6df1bb6ca3f85dd27
Daniel Bronsky <dragonvedb@gmail.com>
2022-12-22 21:04:42 +0300

    Make it an arrow function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,4 @@
-const palindromes = function (str) {
+const palindromes = str => {
     str = str.toLowerCase();
     str = [...str];
 
@@ -16,12 +16,6 @@ const palindromes = function (str) {
         }
         return true;
     })
-
-    /*for (let i = 0; i < (str.length - 1) - i; i++) {
-        if (str[i] != str[str.length - 1 - i]) {
-            return false;
-        }
-    }*/
 
     return ([...str].reverse().toString() == str.toString())
 }
